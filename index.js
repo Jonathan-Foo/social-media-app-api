b5a8507ffe1d8fbcc351385b5f21ef2f4306bee1
Jonathan Foo <jonathan.foozh@gmail.com>
2022-09-13 20:02:49 +1000

    Fixed URI leak

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import postRoutes from './routes/post.js';
-import userRoutes from './routes/users.js';
-import dotenv from 'dotenv'; 
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/post.js";
+import userRoutes from "./routes/users.js";
+import dotenv from "dotenv";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
-app.use(express.urlencoded({extended: true, limit: '30mb'}));
-app.use(express.json({limit: '30mb'}));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
+app.use(express.json({ limit: "30mb" }));
 app.use(cors());
-app.use('/posts', postRoutes); 
-app.use('/users', userRoutes); 
+app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello to Memories API');
-})
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
 
-// const CONNECTION_URL = 'mongodb+srv://user:ze87UbCJ2bWlmhvX@cluster0.ikm3cen.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
-    .catch((err) => console.error(err.message));
-
-
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+  )
+  .catch((err) => console.error(err.message));
